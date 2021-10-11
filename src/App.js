c2497e88d30df09012e29f58865d51b1b31f277c
Navdeep Sharma <navdeepsharmatu@gmail.com>
2021-10-11 10:40:18 +0530

    set up remove person logic using prop drilling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import Setup from './tutorial/6-useReducer';
+// import Setup from './tutorial/6-useReducer';
+import Setup from './tutorial/7-prop-drilling/1-prop-drilling';
+
 function App() {
   return (
     <div className="container">
