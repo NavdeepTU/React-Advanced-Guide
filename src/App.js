8d32a10257623825b2e946271b88872c1123755a
Navdeep Sharma <navdeepsharmatu@gmail.com>
2021-10-13 11:14:36 +0530

    fixed prop drilling problem React's context API anf useContext hook

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 // import Setup from './tutorial/6-useReducer';
-import Setup from './tutorial/7-prop-drilling/1-prop-drilling';
+// import Setup from './tutorial/7-prop-drilling/1-prop-drilling';
+import Setup from './tutorial/8-useContext/1-context-api';
 
 function App() {
   return (
