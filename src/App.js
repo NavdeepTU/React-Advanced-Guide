15589bc2250fbd30aa03bd6acd0a167aaa9f682e
Navdeep Sharma <navdeepsharmatu@gmail.com>
2021-10-14 11:50:08 +0530

    setup useFetch custom hook

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 // import Setup from './tutorial/6-useReducer';
 // import Setup from './tutorial/7-prop-drilling/1-prop-drilling';
-import Setup from './tutorial/8-useContext/1-context-api';
+// import Setup from './tutorial/8-useContext/1-context-api';
+import Setup from './tutorial/9-custom-hooks/1-fetch-example';
 
 function App() {
   return (
