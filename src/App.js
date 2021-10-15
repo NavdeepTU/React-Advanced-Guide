0664cde700cc4496a4491dff38fa5a9726ef914c
Navdeep Sharma <navdeepsharmatu@gmail.com>
2021-10-15 09:12:20 +0530

    setting up propTypes and default props for a component

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React from 'react';
 // import Setup from './tutorial/6-useReducer';
 // import Setup from './tutorial/7-prop-drilling/1-prop-drilling';
 // import Setup from './tutorial/8-useContext/1-context-api';
-import Setup from './tutorial/9-custom-hooks/1-fetch-example';
+// import Setup from './tutorial/9-custom-hooks/1-fetch-example';
+import Setup from './tutorial/10-prop-types/';
 
 function App() {
   return (
