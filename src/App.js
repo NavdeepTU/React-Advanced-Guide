8138428f467e0e2f4895d0967e6e2c176bc60bf2
Navdeep Sharma <navdeepsharmatu@gmail.com>
2021-10-26 19:45:12 +0530

    react router basic setup

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import React from 'react';
 // import Setup from './tutorial/7-prop-drilling/1-prop-drilling';
 // import Setup from './tutorial/8-useContext/1-context-api';
 // import Setup from './tutorial/9-custom-hooks/1-fetch-example';
-import Setup from './tutorial/10-prop-types/';
+// import Setup from './tutorial/10-prop-types/';
+import Setup from './tutorial/11-react-router/setup';
 
 function App() {
   return (
