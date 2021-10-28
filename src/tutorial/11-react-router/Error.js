fc2a1f98c766e80e49d8d2547600d779348dad6d
Navdeep Sharma <navdeepsharmatu@gmail.com>
2021-10-28 15:47:58 +0530

    added react router links

--- src/tutorial/11-react-router/Error.js
+++ src/tutorial/11-react-router/Error.js
@@ -3,7 +3,11 @@ import { Link } from 'react-router-dom';
 const Error = () => {
   return (
     <div>
-      <h1>Error Page</h1>
+      <h1> Error Page </h1>{' '}
+      <Link to="/" className="btn">
+        {' '}
+        Back Home{' '}
+      </Link>{' '}
     </div>
   );
 };
